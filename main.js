8e42a01652b327af462b46235fc272ab57ee360a
domagojkuzmek <domagoj.kuzmek421@gmail.com>
2023-12-23 16:57:57 +0100

    Frontend mentor project

---
+++ main.js
@@ -0,0 +1,35 @@
+import './scss/style.scss';
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const btnOpener = document.querySelector(".btn-open");
+    const btnCloser = document.querySelector(".btn-close");
+    const navigation = document.querySelector("nav");
+    let windowWidth = window.innerWidth;
+
+    //When the page loads it hides the mobile navigation
+    if(windowWidth < 740) {
+        navigation.classList.add("visibility");
+    }
+    // On hmaburger icon show navigation
+    btnOpener.addEventListener("click", () => {
+        navigation.classList.toggle("visibility");
+    })
+    // On remove icon hides the navigation
+    btnCloser.addEventListener("click", () => {
+        navigation.classList.toggle("visibility");
+    })
+
+    // On resize show/hide mobile or desktop navigation 
+    window.addEventListener("resize", () => {
+        if(windowWidth > 740) {
+        navigation.classList.remove("visibility");
+        }
+        location.reload();
+    })
+    // On orientation show/hide mobile or desktop navigation
+    window.addEventListener("orientationchange", () => {
+        navigation.classList.toggle("visibility");
+    })
+})
+
